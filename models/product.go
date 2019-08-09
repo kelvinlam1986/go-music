f5c8d5a737e419397709bcbdb8ce4a319e421461
Minh Lam Su <kelvincoder@gmail.com>
2019-08-09 14:43:30 +0700

    Refactor product model

--- models/product.go
+++ models/product.go
@@ -6,10 +6,10 @@ type Product struct {
 	gorm.Model
 	Image string
 	ImageAlt string `gorm:"column:image_alt"`
-	Price float64 `json:"price"`
-	Promotion float64 `json:"promotion"`
+	Price float64
+	Promotion float64
 	ProductName string `gorm:"column:product_name"`
-	Description string `json:"description"`
+	Description string
 }
 
 func (Product) TableName() string {
